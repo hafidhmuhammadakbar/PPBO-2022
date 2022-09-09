a6dce85297c9b91ba9209916c66ca96b5b14dde4
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2022-09-09 18:44:15 +0700

    praktikum 3

---
+++ Praktikum 3/DemoIf.java
@@ -0,0 +1,8 @@
+public class DemoIf {
+    public static void main(String[] args) {
+        double IPK = 3.5;
+        if (IPK > 3.25) {
+            System.out.println("IPK tinggi");
+        }
+    }    
+}
