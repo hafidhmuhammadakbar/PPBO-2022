30faaf056ec4e0c8f276727da72125a6e5316d5a
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2022-09-10 12:54:13 +0700

    praktikum 2

--- Praktikum 2/PPBO_02_Latihan4.java
+++ Praktikum 2/PPBO_02_Latihan4.java
@@ -1,20 +1,26 @@
 // Nama : Hafidh Muhammad Akbar
 // NIM  : M0521026
 public class PPBO_02_Latihan4 {
+
     public static void main(String[] args) {
         Person dummy1 = new Person();
         Person dummy2 = new Person();
         Person dummy3 = new Person();
-        System.out.println( "Jumlah objek Person yang telah terbuat: "/* Lengkapi kode disini */);
+        System.out.println( "Jumlah objek Person yang telah terbuat: "+Person.count);
         
         Person dummy4 = new Person();
-        System.out.println( "Jumlah objek Person yang telah terbuat: "/* Lengkapi kode disini */);
+        System.out.println( "Jumlah objek Person yang telah terbuat: "+Person.count);
     }
 }
 
 
 class Person
 {
+    static int count;
+
+    Person(){
+        count++;
+    }
     /*
         Tulis kode kalian di sini 
     */
