0141b82fba6b258a47774559127dbe7f8d162508
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2022-09-28 20:32:12 +0700

    Praktikum 4 - Sabtu, 1 Okt 2022 (23.59 WIB)

---
+++ Praktikum4/PPBO_04_Latihan4.java
@@ -0,0 +1,40 @@
+// Nama : Hafidh Muhammad Akbar
+// NIM  : M0521026
+import java.util.Scanner;
+
+public class PPBO_04_Latihan4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = 10;
+        try {
+            while(true){
+                System.out.println("Please input non-integer to trigger exception: ");
+                x = scanner.nextInt();
+            }
+        } catch (Exception e) {
+            StackTraceElement[] trace = e.getStackTrace();
+            int lengthTrace = trace.length; 
+
+            int exceptionLineNumber = trace[(lengthTrace-1)].getLineNumber(); // simpan angka exception line number di variabel ini
+            System.out.println("Whoops! exception occured on line: " + exceptionLineNumber);
+            
+            //e.printStackTrace();
+        }
+
+        scanner.close();
+    }
+}
+
+//      Pada latihan ini kalian diminta untuk mencetak exception line number atau baris ke berapa penyebab 
+// terjadinya exception. Sebelumnya kalian sudah mengenal method printStackTrace() yang akan mencetak 
+// urutan stack exception. Pada baris terakhir stack trace, terdapat informasi letak exception line number 
+// yang terjadi pada source code ini. 
+// Akses exception line number tersebut dan simpan pada variabel exceptionLineNumber!
+
+// Hint:
+//  - selidiki method dan field yang tersedia
+//  - proses pencarian dimulai dari mengakses stack trace
+//  - trace yang diperlukan berada di paling akhir stack
+
+// Output yang diharapkan:
+// Whoops! exception occured on line: 12
