45acb1de263aef9e078fd9762f80de92cb173726
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2022-10-02 22:23:08 +0700

    Praktikum 5 - Minggu, 9 Okt 2022 (23:59)

---
+++ Praktikum5/HackerrankNo2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HackerrankNo2 {
+    public static void main(String args[] ){
+        String inputString;
+        int tipe;
+        Scanner scan = new Scanner(System.in);
+
+        inputString = scan.nextLine();
+        scan.close();
+
+        if(inputString.matches("^[AIUEOaiueo].*")){
+            tipe = 1;
+        }
+        else if(inputString.matches("^(.*[^ng])(ng)")){
+            tipe = 2;
+        }
+        else{
+            tipe = -1;
+        }
+        System.out.print(tipe);
+    }
+}
